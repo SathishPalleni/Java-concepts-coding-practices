daa1c5024b0f445512efa7224f18ab9617d97f98
Psatish2000 <162052299+Psatish2000@users.noreply.github.com>
2026-07-12 19:05:39 +0530

    interview

---
+++ src/Interview.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Interview {
+
+    public static void main(String [] args){
+        ArrayList<int[]> arr =  new ArrayList(1,2,3,4,5);
+        int [] result = {1,3,5,6};
+
+    }
+}
